1a3791342d22283c6399916efd3dd548f697f046
Mauro Lepore <maurolepore@gmail.com>
2021-04-13 08:07:41 -0500

    Implement load_file

---
+++ R/load_file.R
@@ -0,0 +1,9 @@
+load_file <- function(file) {
+  ext <- tools::file_ext(file)
+  switch(
+    ext,
+    csv = vroom::vroom(file, delim = ",", col_types = list()),
+    tsv = vroom::vroom(file, delim = "\t", col_types = list()),
+    stop("Invalid extension: ", ext)
+  )
+}
