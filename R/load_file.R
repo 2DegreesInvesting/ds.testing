53d790cf7066bb6940a9894d7b8c98a8397d5a28
Mauro Lepore <maurolepore@gmail.com>
2021-04-20 07:17:21 -0500

    Defactor

--- R/load_file.R
+++ R/load_file.R
@@ -1,10 +1,13 @@
 load_file <- function(file) {
   ext <- tools::file_ext(file)
 
-  switch(
-    ext,
-    csv =  vroom::vroom(file, delim = ",", col_types = list()),
-    tsv =  vroom::vroom(file, delim = "\t", col_types = list()),
-    stop("Invalid file")
-  )
+  if (identical(ext, "csv")) {
+    return(vroom::vroom(file, delim = ",", col_types = list()))
+  }
+
+  if (identical(ext, "tsv")) {
+    return(vroom::vroom(file, delim = "\t", col_types = list()))
+  }
+
+  stop("Invalid file")
 }
