5d21e8c3ea039e0c84a6aead56ecf83bfbf03cd8
Mauro Lepore <maurolepore@gmail.com>
2021-04-20 07:23:39 -0500

    Defactor

--- R/load_file.R
+++ R/load_file.R
@@ -1,12 +1,12 @@
 load_file <- function(file) {
   ext <- tools::file_ext(file)
 
-  if (identical(ext, "csv")) {
-    return(vroom::vroom(file, delim = ",", col_types = list()))
-  }
-
   if (identical(ext, "tsv")) {
     return(vroom::vroom(file, delim = "\t", col_types = list()))
+  }
+
+  if (identical(ext, "csv")) {
+    return(vroom::vroom(file, delim = ",", col_types = list()))
   }
 
   stop("Invalid file")
--- tests/testthat/test-load_file.R
+++ tests/testthat/test-load_file.R
@@ -1,7 +1,3 @@
-test_that("errors gracefully", {
-  expect_error(load_file("blah"), "Invalid file")
-})
-
 test_that("reads a .csv", {
   df <- tibble::tibble(x = 1, y = 2)
   file <- tempfile(fileext = ".csv")
@@ -10,28 +6,4 @@ test_that("reads a .csv", {
   out <- load_file(file)
 
   expect_equal(out, df)
-})
-
-test_that("reads a .tsv", {
-  # Arrange
-  df <- tibble::tibble(x = 1, y = 2)
-  file <- tempfile(fileext = ".tsv")
-  vroom::vroom_write(df, file, delim = "\t")
-
-  # Act
-  out <- load_file(file)
-
-  # Assert
-  expect_equal(out, df)
-})
-
-test_that("throws no message", {
-  df <- tibble::tibble(x = 1, y = 2)
-  file_csv <- tempfile(fileext = ".csv")
-  file_tsv <- tempfile(fileext = ".tsv")
-  vroom::vroom_write(df, file_csv, delim = ",")
-  vroom::vroom_write(df, file_tsv, delim = "\t")
-
-  expect_message(load_file(file_csv), NA)
-  expect_message(load_file(file_tsv), NA)
 })
